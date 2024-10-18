6e410250dbc7e21095bff1d367c7a2914cea14be
vipin singh <vipin.singh@vensysco.com>
2024-10-18 12:08:23 +0530

    fixed changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -120,8 +120,8 @@ export default function Home() {
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> */}
       </Head>
       {console.log("appIdFetched",appIdFetched)}
-
-      {appIdFetched && "<RoutePage />"} {/* Only render RoutePage after app ID is fetched */}
+      
+    <RoutePage /> {/* Only render RoutePage after app ID is fetched */}
     </>
   );
 }
