3d72ac72b8a0231a748f143dee0e85318905dc39
vipin singh <vipin.singh@vensysco.com>
2024-10-23 18:21:21 +0530

    fixed changes

--- src/pages/view-courses/[onlineCourseID].js
+++ src/pages/view-courses/[onlineCourseID].js
@@ -14,75 +14,69 @@ import Loader from "@/component/loader";
 
 const OC_image = "/assets/images/courseRightImg.svg";
 
-const OnlineCourse = ({initialTab}) => {
+const OnlineCourse = ({ initialTab }) => {
   const [showError, setShowError] = useState(false);
   const [key, setKey] = useState("Course Detail");
-  const [onlineCourse, setOnlineCourse] = useState('')
+  const [onlineCourse, setOnlineCourse] = useState('');
   const [id, setId] = useState('');
   const [titleName, setTitleName] = useState('');
   const [cat_description, setCat_description] = useState('');
-
   const resetPdfLayerRef = useRef();
   const resetCourseCurriculumLayerRef = useRef();
   const Router = useRouter();
   const { onlineCourseID } = Router.query || "Live Course:6244";
-  const courseTypeData = useSelector((state) => state.allCategory?.allCategory?.course_type_master)
+  const courseTypeData = useSelector((state) => state.allCategory?.allCategory?.course_type_master);
 
-  console.log("onlineCourseID",onlineCourseID)
-  console.log('courseTypeData', courseTypeData)
-
-  // const id = onlineCourseID.slice(onlineCourseID.indexOf(':') +1, onlineCourseID.length)
-  // const titleName = onlineCourseID.slice(0, onlineCourseID.indexOf(':'))
-  // localStorage.setItem()
+  console.log("onlineCourseID", onlineCourseID);
+  console.log('courseTypeData', courseTypeData);
 
   useEffect(() => {
     const currentPath = Router.asPath;
     localStorage.setItem("redirectdetails", currentPath);
-    setShowError(false)
+    setShowError(false);
     if (onlineCourseID) {
-      // window.scrollTo(0, 0);
-      fetchCourseDetail(onlineCourseID.slice(onlineCourseID.indexOf(':') +1, onlineCourseID.length));
-      setId(onlineCourseID.slice(onlineCourseID.indexOf(':') +1, onlineCourseID.length))
-      setTitleName(onlineCourseID.slice(0, onlineCourseID.indexOf(':')))
+      const id = onlineCourseID.slice(onlineCourseID.indexOf(':') + 1);
+      const title = onlineCourseID.slice(0, onlineCourseID.indexOf(':'));
+      setId(id);
+      setTitleName(title);
+      fetchCourseDetail(id);
     }
-    
-  }, [onlineCourseID, courseTypeData]);
+  }, [onlineCourseID]);
 
   useEffect(() => {
-    if(titleName) {
-      setCat_description(courseTypeData && courseTypeData.filter((item) => item.name == titleName))
+    if (titleName && courseTypeData) {
+      const filteredData = courseTypeData.filter(item => item.name === titleName);
+      setCat_description(filteredData);
     }
-  }, [titleName])
+  }, [titleName, courseTypeData]);
 
   const fetchCourseDetail = async (id) => {
-    try{
+    try {
       const token = get_token();
       const formData = {
-        'course_type':id,
-        'page':1,
-        'sub_cat':1,
-        'main_cat':0,
-        // 'course_ids': id
-      }
-      const response_getCourse_service = await getCourse_service(encrypt(JSON.stringify(formData),token));
-      const response_getCourse_data = decrypt(response_getCourse_service.data, token)
-      console.log('response_getCourse_data', response_getCourse_data)
+        'course_type': id,
+        'page': 1,
+        'sub_cat': 1,
+        'main_cat': 0,
+      };
+      const response_getCourse_service = await getCourse_service(encrypt(JSON.stringify(formData), token));
+      const response_getCourse_data = decrypt(response_getCourse_service.data, token);
+      console.log('response_getCourse_data', response_getCourse_data);
+
       if (response_getCourse_data.status) {
-        if(response_getCourse_data?.data?.length == 0){
-          setShowError(true)
+        if (response_getCourse_data?.data?.length === 0) {
+          setShowError(true);
+        } else {
+          setOnlineCourse(response_getCourse_data?.data);
         }
-        else setOnlineCourse(response_getCourse_data?.data);
-        // console.log('detail', response_getCourse_data.data);
-      }
-      else {
-        setShowError(true)
+      } else {
+        setShowError(true);
       }
     } catch (error) {
-      console.log("error found: ", error)
-      // router.push('/')
+      console.log("error found: ", error);
+      // Optionally handle error, e.g., redirect to an error page or show a message
     }
   };
-
 
   return (
     <>
@@ -90,65 +84,39 @@ const OnlineCourse = ({initialTab}) => {
       <div className="container-fluid p-0 mt-5">
         <div 
           className={
-            titleName && (titleName == "Bookstore" || titleName == "e-BOOK" || titleName == "Books")
+            titleName && (titleName === "Bookstore" || titleName === "e-BOOK" || titleName === "Books")
               ? `bookStoreContainer row`
               : `course_Container row`
           }
         >
           <div className="col-md-12 m-0" style={{ paddingTop: "15px" }}>
-            <nav aria-label="breadcrumb ">
+            <nav aria-label="breadcrumb">
               <ol className="breadcrumb mb-0 cursor">
-                <li
-                  className="breadcrumb-item"
-                  onClick={() => Router.back()}
-                >
+                <li className="breadcrumb-item" onClick={() => Router.back()}>
                   {`Home`}
                   <i className="bi bi-chevron-right"></i>
-                  {/* <Icon.ChevronRight /> */}
                 </li>
-                <li
-                  className="breadcrumb-item active"
-                  // onClick={() => navigate("/view-courses")}
-                >
+                <li className="breadcrumb-item active">
                   {`${titleName}`}
                   <i className="bi bi-chevron-right"></i>
-                  {/* <Icon.ChevronRight /> */}
                 </li>
-                {/* {courseDetails && <li className="breadcrumb-item highlight">{courseDetails.course_detail.title}</li>} */}
               </ol>
             </nav>
-            </div>
-            <div 
-              className={`col-sm-12 col-md-8
-                ${
-                  titleName && (titleName == "Bookstore" || titleName == "e-BOOK" || titleName == "Books")
-                    ? `col-lg-6`
-                    : `col-lg-8`
-                }`}
-            >
+          </div>
+          <div 
+            className={`col-sm-12 col-md-8 ${titleName && (titleName === "Bookstore" || titleName === "e-BOOK" || titleName === "Books") ? `col-lg-6` : `col-lg-8`}`}
+          >
             <div className="onlineCourseTitle"> 
               <p className="mb-1 title">{titleName}</p>
-              <p className="onlineCourseDetail" 
-              // contentEditable='true' 
-              dangerouslySetInnerHTML={{ __html: cat_description && cat_description[0]?.description }}>
-              </p>
+              <p className="onlineCourseDetail" dangerouslySetInnerHTML={{ __html: cat_description && cat_description[0]?.description }} />
             </div>
           </div>
           <div 
-            className={`col-sm-12 col-md-4 d-none d-sm-none d-md-none d-lg-block course_imageContainer  ${
-              titleName && (titleName == "Bookstore" || titleName == "e-BOOK" || titleName == "Books")
-                ? `col-lg-6`
-                : `col-lg-4`
-            }`}
+            className={`col-sm-12 col-md-4 d-none d-sm-none d-md-none d-lg-block course_imageContainer ${titleName && (titleName === "Bookstore" || titleName === "e-BOOK" || titleName === "Books") ? `col-lg-6` : `col-lg-4`}`}
           >
             <div className="imgContainer">
-            {titleName &&
-              (titleName == "Bookstore" || titleName == "e-BOOK" || titleName == "Books") ? (
-                <img
-                  className="bookImg pb-4"
-                  src="/assets/images/bookStoreRightImg.svg"
-                  alt=""
-                />
+              {titleName && (titleName === "Bookstore" || titleName === "e-BOOK" || titleName === "Books") ? (
+                <img className="bookImg pb-4" src="/assets/images/bookStoreRightImg.svg" alt="" />
               ) : (
                 <img src={OC_image} alt="" />
               )}
@@ -156,20 +124,20 @@ const OnlineCourse = ({initialTab}) => {
           </div>
         </div>
         <div className="course_cardContainer onlineCourse mb-3">
-              <div className="row">
-                {onlineCourse?.length > 0 ? 
-                  onlineCourse.map((item, index) => {
-                    return <Card1 value = {item} titleName = {titleName} key={index} />
-                  })
-                  :
-                  showError ? 
-                    <ErrorPage />
-                    :
-                    <Loader />
-                }
-              </div>
-            </div>
+          <div className="row">
+            {onlineCourse?.length > 0 ? 
+              onlineCourse.map((item, index) => (
+                <Card1 value={item} titleName={titleName} key={index} />
+              ))
+              :
+              showError ? 
+                <ErrorPage />
+                :
+                <Loader />
+            }
+          </div>
         </div>
+      </div>
       <Footer />
     </>
   );
@@ -182,7 +150,7 @@ export const getStaticPaths = async () => {
     sub_cat: 1,
     main_cat: 0,
   };
-  
+
   const response_getCourse_service = await getCourse_service(encrypt(JSON.stringify(formData), token));
   const response_getCourse_data = decrypt(response_getCourse_service.data, token);
 
@@ -191,7 +159,7 @@ export const getStaticPaths = async () => {
   }
 
   const paths = response_getCourse_data.data.map(course => ({
-    params: { onlineCourseID: `${course.name} + ':' + ${course.id}` }, // Adjust according to your data structure
+    params: { onlineCourseID: `${course.name}:${course.id}` }, // Adjust according to your data structure
   }));
 
   return { paths, fallback: false }; // `false` means 404 for unmatched paths
@@ -206,6 +174,5 @@ export const getStaticProps = async ({ params }) => {
     },
   };
 };
-
 
 export default OnlineCourse;
